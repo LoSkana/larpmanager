db8c326f2e21b1c9c40260a7f6b6503dbda00b12
loskana <scanagatta.mauro@gmail.com>
2025-05-22 23:27:00 +0200

    DONE!

--- larpmanager/templates/elements/form/char_highlight.js
+++ larpmanager/templates/elements/form/char_highlight.js
@@ -4,15 +4,48 @@
 
 var tinymce_count = ["teaser", "text", "e"];
 
+function openQtip(pageX, pageY, x, y, content) {
+    $('body').append('<div id="qtip-cursor-helper" style="position:absolute;left:' + pageX + 'px;top:' + pageY + 'px;width:1px;height:1px;"></div>');
+    $('#qtip-cursor-helper').qtip({
+        content: { text: content },
+        style: {
+            classes: 'qtip-dark qtip-rounded qtip-shadow'
+        }, show: {
+            ready: true,
+            effect: function(offset) {
+                $(this).fadeIn(500);
+            }
+        }, hide: {
+            event: 'mouseleave unfocus click',
+            delay: 200,
+            effect: function(offset) {
+                $(this).fadeOut(500);
+            }
+        }, position: {
+            my: 'top left',
+            at: 'bottom center',
+            target: [x, y]
+        }, events: {
+            hide: function() {
+                $('#qtip-cursor-helper').remove();
+            }
+        }
+    });
+}
+
 function setUpHighlight(key) {
     const editor = tinymce.get(key);
     if (!editor) return;
     editor.on('selectionchange', function(e) {
+        $('#qtip-cursor-helper').remove();
+        $('.qtip').qtip('hide');
+
         var selected = editor.selection.getContent({ format: 'text' });
         if (!selected) return;
 
         // If it is not exactly #XX (where X are numbers) then exit
-        if (!/^#\d+$/.test($(element).text())) return;
+        selected = $.trim(selected);
+        if (!/^#\d+$/.test(selected)) return;
 
         var win = editor.getWin();
         var sel = win.getSelection ? win.getSelection() : editor.getDoc().selection;
@@ -22,12 +55,34 @@ function setUpHighlight(key) {
         if (range && !range.collapsed) {
             var rect = range.getBoundingClientRect();
             var iframeRect = editor.iframeElement.getBoundingClientRect();
-            var pageX = rect.left + iframeRect.left;
-            var pageY = rect.top + iframeRect.top;
-            console.log('Page X:', pageX, 'Page Y:', pageY);
-        }
 
-        console.log(selected);
+            // Scroll in iframe
+            var iframeScroll = editor.getDoc().documentElement.scrollTop || editor.getDoc().body.scrollTop;
+
+            // Scroll in page
+            var pageScrollY = window.scrollY || window.pageYOffset;
+            var pageScrollX = window.scrollX || window.pageXOffset;
+
+            // Final computation
+            var pageX = rect.left + iframeRect.left + pageScrollX;
+            var pageY = rect.top + iframeRect.top + pageScrollY + iframeScroll;
+            var x = rect.left + (rect.width / 2) + iframeRect.left + pageScrollX;
+            var y = rect.bottom + iframeRect.top + pageScrollY + iframeScroll;
+
+            // console.log('Page X:', pageX, 'Page Y:', pageY);
+            // console.log('x:', x, 'y:', y);
+
+            request = $.ajax({
+                url: "{% url 'show_char' event.slug run.number %}",
+                method: "POST",
+                data: { text: selected },
+                datatype: "json",
+            });
+
+            request.done(function(res) {
+                openQtip(pageX, pageY, x, y, res.content);
+            });
+        }
     });
 }
 
