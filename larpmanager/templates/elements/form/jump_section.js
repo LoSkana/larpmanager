bc3bd8f03f7b87224f8c690b197ceadaa2457815
loskana <scanagatta.mauro@gmail.com>
2025-06-20 18:43:42 +0200

    completed

--- larpmanager/templates/elements/form/jump_section.js
+++ larpmanager/templates/elements/form/jump_section.js
@@ -1,13 +1,13 @@
 {% load tz show_tags static  i18n %}
 
-{% if jump_section %}
+{% if form.jump_section %}
 
 <script>
 
 window.addEventListener('DOMContentLoaded', function() {
     $(document).ready(function(){
         setTimeout(()=> {
-            $('a.my_toggle[tog="{{ jump_section }}"]').trigger('click');
+            $('a.my_toggle[tog="sec_{{ form.jump_section | slugify }}"]').trigger('click');
         }, 100);
     });
 });
