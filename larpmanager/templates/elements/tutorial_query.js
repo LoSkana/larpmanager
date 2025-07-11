f82e6004cd7093e4816d08df759a964525ac0165
loskana <scanagatta.mauro@gmail.com>
2025-07-11 11:00:23 +0200

    added links in what would you like to do

--- larpmanager/templates/elements/tutorial_query.js
+++ larpmanager/templates/elements/tutorial_query.js
@@ -20,6 +20,12 @@ function slugify(text) {
         .replace(/-+$/, '');
 }
 
+{% if run %}
+    let run_id = '{{ run.id }}';
+{% else %}
+    let run_id = '0';
+{% endif %}
+
 function search_tutorial() {
     const query = $('#tutorial_query').val().trim();
     if (query === '') return;
@@ -27,20 +33,38 @@ function search_tutorial() {
     request = $.ajax({
         url: "{% url 'tutorial_query' %}",
         method: "POST",
-        data: {'q': query},
+        data: {'q': query, 'r': run_id},
         datatype: "json",
     });
 
     request.done(function(data) {
-        result = '';
-        data.forEach(item => {
-            result += '<tr><td><h3>' + item.title + ' - ' + item.section + '</h3></td><td>' +
+        // links text
+        link_text = '';
+        data.links.forEach(item => {
+            link_text += '<tr><td><h3>' + item.name + '</h3></td><td>' +
+            '<td><p><a href="' + item.href + '" target="_blank"><i>' + item.descr + ' </i></a></p></td></tr>';
+        });
+
+        if (link_text.trim() !== "") {
+            link_text = '<h2>Links</h2><table class="no_csv">' + link_text + '</table>';
+        }
+
+        // tutorials text
+        tutorial_text = '';
+        data.tutorials.forEach(item => {
+            tutorial_text += '<tr><td><h3>' + item.title + ' - ' + item.section + '</h3></td><td>' +
             '<td><p><a href="' + tutorials_url + item.slug + '#' + slugify(item.section) + '" target="_blank"><i>' + item.snippet + ' [...]</i></a></p></td></tr>';
         });
 
+        if (tutorial_text.trim() !== "") {
+            tutorial_text = '<h2>Tutorials</h2><table class="no_csv">' + tutorial_text + '</table>';
+        }
+
+        // prepare results
+        result = link_text + tutorial_text
+
         if (result.trim() === "")
             result = "{% trans "No results found; please try with more simpler terms (remember to write in English)" %}";
-        else result = '<h2>{% trans "Results" %}</h2><table class="no_csv">' + result + '</table>';
 
         uglipop({class:'popup_query', source:'html', content: result});
     });
--- larpmanager/utils/tutorial_query.py
+++ larpmanager/utils/tutorial_query.py
@@ -1,14 +1,20 @@
 import os
+from difflib import SequenceMatcher
 
 import deepl
 from bs4 import BeautifulSoup
 from django.conf import settings as conf_settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from django.http import JsonResponse
+from django.urls import reverse
 from whoosh.fields import ID, TEXT, Schema
 from whoosh.index import create_in, open_dir
 from whoosh.qparser import QueryParser
 
 from larpmanager.mail.base import notify_admins
+from larpmanager.models.access import AssocPermission, EventPermission
+from larpmanager.models.event import Run
 from larpmanager.models.larpmanager import LarpManagerTutorial
 from larpmanager.utils.tasks import background_auto
 
@@ -62,8 +68,20 @@ def delete_index(tutorial_id):
     writer.commit()
 
 
+def similarity(a, b):
+    return SequenceMatcher(None, a.lower(), b.lower()).ratio()
+
+
+def get_sorted_permissions(model, query):
+    permissions = model.objects.filter(Q(name__icontains=query) | Q(descr__icontains=query)).values(
+        "name", "slug", "descr"
+    )
+    return sorted(permissions, key=lambda p: similarity(p["name"], query), reverse=True)
+
+
 def query_index(request):
     orig_string = request.POST.get("q", "")
+    run_id = int(request.POST.get("r", "0"))
 
     # translate it
     translator = deepl.Translator(conf_settings.DEEPL_API_KEY)
@@ -72,13 +90,35 @@ def query_index(request):
     # notify admins
     notify_admins(f"query_index: {query_string}", f"{orig_string} - {request.user}")
 
-    # search for it
+    # get links
+    if run_id:
+        try:
+            run = Run.objects.select_related("event").get(pk=run_id, event__assoc_id=request.assoc["id"])
+        except ObjectDoesNotExist:
+            return JsonResponse({})
+
+        sorted_permissions = get_sorted_permissions(EventPermission, query_string)
+        links = [
+            {
+                "name": perm["name"],
+                "descr": perm["descr"],
+                "href": reverse(perm["slug"], args=[run.event.slug, run.number]),
+            }
+            for perm in sorted_permissions
+        ]
+    else:
+        sorted_permissions = get_sorted_permissions(AssocPermission, query_string)
+        links = [
+            {"name": perm["name"], "descr": perm["descr"], "href": reverse(perm["slug"])} for perm in sorted_permissions
+        ]
+
+    # get tutorials
     ix = get_or_create_index(INDEX_DIR)
     with ix.searcher() as searcher:
         query = QueryParser("content", ix.schema).parse(query_string)
         results = searcher.search(query, limit=10)
-        data = [
+        tutorials = [
             {"slug": r["slug"], "title": r["title"], "section": r["section_title"], "snippet": r["content"][:300]}
             for r in results
         ]
-    return JsonResponse(data, safe=False)
+    return JsonResponse({"tutorials": tutorials, "links": links}, safe=False)
