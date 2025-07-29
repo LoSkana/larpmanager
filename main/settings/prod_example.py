baf976fd7e34d7221328a0d25c24fb9189c5b6aa
loskana <scanagatta.mauro@gmail.com>
2025-07-29 23:28:52 +0200

    corrected

--- main/settings/prod_example.py
+++ main/settings/prod_example.py
@@ -36,8 +36,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "../media")
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "../static")
 
-BACKUP_ROOT = os.path.join(BASE_DIR, "../backup")
-
 COMPRESS_ROOT = STATIC_ROOT
 COMPRESS_URL = STATIC_URL
 
