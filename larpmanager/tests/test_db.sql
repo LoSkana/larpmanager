b1aed58196e2345d754b44cbc094d7ff72bacf70
loskana <scanagatta.mauro@gmail.com>
2025-08-26 09:40:18 +0200

    correct last reference

--- larpmanager/tests/test_db.sql
+++ larpmanager/tests/test_db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23fd7f80f2e86b3aaecb8a763d17c4e56c3098813ef913f800306f03d7cdfcc6
-size 620464
+oid sha256:ef6dd20c76b2bb393607e8ccca0d5d5f63c0523528ff85a16e0fff27a3c83943
+size 620408
