b3763c51297aa916f6c68c72d5461dc501a94245
Scott Spalding <spaldingscott@gmail.com>
2025-09-02 22:23:48 +0000

    Can view pool types

--- larpmanager/views/orga/characterinventory.py
+++ larpmanager/views/orga/characterinventory.py
@@ -20,22 +20,22 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from larpmanager.forms.characterinventory import OrgaPoolTypePxForm
+from larpmanager.models.characterinventory import PoolTypeCI
 from larpmanager.utils.edit import orga_edit
 from larpmanager.utils.event import check_event_permission
 
 
 @login_required
 def orga_ci_character_inventory(request, s, n):
-    #return HttpResponse("Hello World: orga_ci_character_inventory is wired up correctly.")
     ctx = check_event_permission(request, s, n, "orga_ci_character_inventory")
-    #ctx["px_user"] = ctx["event"].get_config("px_user", False)
-    #ctx["list"] = ctx["event"].get_elements(AbilityPx).order_by("number")
+    ctx["list"] = ctx["event"].get_elements(PoolTypeCI).order_by("number")
     return render(request, "larpmanager/orga/ci/character_inventories.html", ctx)
 
 
 @login_required
 def orga_ci_pool_types(request, s, n):
     ctx = check_event_permission(request, s, n, "orga_ci_pool_types")
+    ctx["list"] = ctx["event"].get_elements(PoolTypeCI).order_by("number")
     return render(request, "larpmanager/orga/ci/ci_pool_types.html", ctx)
 
 
