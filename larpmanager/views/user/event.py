4733b09bc5cd6fb14dd902cc5b1803d829f1edc1
mauro <mauro.scanagatta@spaziodati.eu>
2025-10-06 11:42:15 +0200

    small fix

--- larpmanager/views/user/event.py
+++ larpmanager/views/user/event.py
@@ -382,7 +382,7 @@ def event(request, s):
 
     ctx["data"] = ctx["event"].show()
 
-    ctx["no_robots"] = datetime.today().date() > ctx["run"].end
+    ctx["no_robots"] = datetime.today().date() > ctx["run"].end or not ctx["run"].development == DevelopStatus.SHOW
 
     return render(request, "larpmanager/event/event.html", ctx)
 
