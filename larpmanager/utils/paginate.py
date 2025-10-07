c9168ee49d1f093e59c5807fa705472e8216092c
mauro <mauro.scanagatta@spaziodati.eu>
2025-10-07 14:10:32 +0200

    fix info show

--- larpmanager/utils/paginate.py
+++ larpmanager/utils/paginate.py
@@ -193,7 +193,7 @@ def _prepare_data_json(ctx, elements, view, edit, exe=True):
         "value": lambda obj: int(obj.value) if obj.value == obj.value.to_integral() else str(obj.value),
         "details": lambda obj: str(obj.details),
         "credits": lambda obj: int(obj.credits) if obj.credits == obj.credits.to_integral() else str(obj.credits),
-        "info": lambda obj: str(obj.info),
+        "info": lambda obj: str(obj.info) if obj.info else "",
         "vat_ticket": lambda obj: obj.vat_ticket.to_integral(),
         "vat_options": lambda obj: obj.vat_options.to_integral(),
     }
