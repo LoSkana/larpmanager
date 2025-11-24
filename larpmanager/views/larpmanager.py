75643ed8a4e289883b3b707f38ccc6800ec53a2c
mauro <mauro.scanagatta@spaziodati.eu>
2025-11-24 13:36:20 +0100

    fixed

--- larpmanager/views/larpmanager.py
+++ larpmanager/views/larpmanager.py
@@ -1085,7 +1085,7 @@ def donate(request: HttpRequest) -> Any:
     return render(request, "larpmanager/larpmanager/donate.html", context)
 
 
-def debug_user(request: HttpRequest, member_id: Any) -> HttpResponse:
+def debug_user(request: HttpRequest, member_id: Any) -> None:
     """Login as a specific user for debugging purposes.
 
     Allows admin users to login as another user for debugging.
@@ -1095,14 +1095,13 @@ def debug_user(request: HttpRequest, member_id: Any) -> HttpResponse:
         request: Django HTTP request object
         member_id: Member ID to login as
 
-    Returns:
-        HttpResponse: Redirect to user's profile page
+    Side effects:
+        Logs in as the specified user
 
     """
     check_lm_admin(request)
     member = Member.objects.get(pk=member_id)
     login(request, member.user, backend=get_user_backend())
-    return redirect("user_profile")
 
 
 @ratelimit(key="ip", rate="5/m", block=True)
