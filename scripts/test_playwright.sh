47be0f409fbfaf31fd9bbab2dd3f6ec526994c8c
mauro <mauro.scanagatta@spaziodati.eu>
2025-12-19 22:45:55 +0100

    updated

--- scripts/test_playwright.sh
+++ scripts/test_playwright.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pytest -n "$WORKERS" larpmanager/tests/playwright
+pytest -n "$WORKERS" --reruns 5 --reruns-delay 2 larpmanager/tests/playwright
 
 # local
 #pytest -n "$WORKERS" --maxfail 1 --reruns-delay 2 larpmanager/tests/playwright
