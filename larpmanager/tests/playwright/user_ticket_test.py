80181d2bdcc10cea246bdb02b471fb7481aab224
mauro <mauro.scanagatta@spaziodati.eu>
2026-01-09 21:41:54 +0100

    fixed tests

--- larpmanager/tests/playwright/user_ticket_test.py
+++ larpmanager/tests/playwright/user_ticket_test.py
@@ -79,7 +79,7 @@ def test_user_ticket(pw_page: Any) -> None:
     submit_confirm(page)
 
     # change email
-    page.get_by_role("link", name=" Profile").click()
+    page.get_by_role("link", name=re.compile("Profile")).click()
     page.get_by_role("link", name="Would you like to change it?").click()
     page.get_by_role("textbox", name="Email").click()
     page.get_by_role("textbox", name="Email").fill("asdsa@dasasd.it")
