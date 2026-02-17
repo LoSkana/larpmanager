12ae3179d46e3fa17d6e7e7ae9a20951cf010262
loskana <scanagatta.mauro@gmail.com>
2026-02-17 12:03:18 +0100

    gogo

--- larpmanager/cache/dirty.py
+++ larpmanager/cache/dirty.py
@@ -97,7 +97,7 @@ def mark_dirty(cache_ns: str, section: str, item_ids: list[int], event_id: int |
     """
     for item_id in item_ids:
         cache.set(get_dirty_key(cache_ns, section, item_id), "1")
-        logger.debug("marking %s %s %d", cache_ns, section, item_id)
+        logger.debug("marking %s %s %s", cache_ns, section, item_id)
     if event_id is not None:
         cache.set(get_has_dirty_key(cache_ns, event_id), "1")
 
