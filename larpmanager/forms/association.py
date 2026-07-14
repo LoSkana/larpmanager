52973c97be87feda2f449eb6321b3c3e3eb3f0ee
loskana <scanagatta.mauro@gmail.com>
2026-07-14 23:34:55 +0200

    fixed

--- larpmanager/forms/association.py
+++ larpmanager/forms/association.py
@@ -80,6 +80,7 @@ class ExeAssociationForm(BaseModelForm):
             "skin",
             "lite_mode",
             "maintainers",
+            "demo_type",
         )
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
