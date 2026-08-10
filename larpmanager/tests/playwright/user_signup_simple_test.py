d6af50ce91a9464d2508ade5623ea088ff68d090
loskana <scanagatta.mauro@gmail.com>
2026-08-10 17:32:15 +0200

    improved translations

--- larpmanager/tests/playwright/user_signup_simple_test.py
+++ larpmanager/tests/playwright/user_signup_simple_test.py
@@ -108,7 +108,7 @@ def help_questions(live_server: Any, page: Any) -> None:
 
     go_to(page, live_server, "/manage/questions")
     page.get_by_role("link", name="Close").click()
-    page.get_by_role("link", name="Show answered questions").click()
+    page.get_by_role("link", name="Show closed questions").click()
     submit_confirm(page)
 
 
