3f02bae59347bd240d5d5a71c949f03a939e6ac5
loskana <scanagatta.mauro@gmail.com>
2026-08-14 14:25:12 +0200

    fixed test

--- larpmanager/tests/unit/test_text_versions.py
+++ larpmanager/tests/unit/test_text_versions.py
@@ -21,6 +21,7 @@
 """Tests for text-version snapshots."""
 
 from larpmanager.cache.question import clear_writing_questions_cache
+from larpmanager.models.form import QuestionApplicable, WritingQuestion, WritingQuestionType
 from larpmanager.models.writing import Plot, PlotCharacterRel, TextVersion, TextVersionChoices
 from larpmanager.tests.unit.base import BaseTestCase
 from larpmanager.utils.edit.backend import save_version
@@ -33,6 +34,14 @@ class TestTextVersionPlotRoles(BaseTestCase):
         """Create a plot and two assigned characters."""
         super().setUp()
         event = self.create_event(name="Text version event")
+        # the character feature is not active in unit fixtures, so no default form is generated
+        WritingQuestion.objects.create(
+            event=event,
+            applicable=QuestionApplicable.CHARACTER,
+            typ=WritingQuestionType.NAME,
+            name="Name",
+            order=1,
+        )
         # drop any questions cached for a previously created event reusing this id
         clear_writing_questions_cache(event.id)
         self.plot = Plot.objects.create(event=event, number=1, name="The lost crown", text="Plot text")
