5385cbff04bb29b5072e5333e9c7684e6b4e5f02
loskana <scanagatta.mauro@gmail.com>
2026-09-16 10:00:22 +0200

    fixed

--- larpmanager/tests/playwright/orga_form_writing_config_test.py
+++ larpmanager/tests/playwright/orga_form_writing_config_test.py
@@ -69,7 +69,7 @@ def feature_fields(page: Any) -> None:
 
     # add config fields - title
     page.get_by_role("link", name="Configuration").first.click()
-    page.get_by_role("link", name=re.compile(r"^Characters ")).click()
+    page.get_by_role("link", name=re.compile(r"^Character Sheet")).click()
     page.locator("#id_writing_title").check()
     submit_confirm(page)
 
@@ -79,7 +79,7 @@ def feature_fields(page: Any) -> None:
 
     # add config fields - cover, assigned
     page.get_by_role("link", name="Configuration").first.click()
-    page.get_by_role("link", name=re.compile(r"^Characters")).click()
+    page.get_by_role("link", name=re.compile(r"^Character Sheet")).click()
     page.locator("#id_writing_title").uncheck()
     page.locator("#id_writing_cover").check()
     page.locator("#id_writing_assigned").check()
@@ -96,7 +96,7 @@ def feature_fields(page: Any) -> None:
 def feature_fields2(page: Any, live_server: Any) -> None:
     # add config hide, assigned
     page.get_by_role("link", name="Configuration").first.click()
-    page.get_by_role("link", name=re.compile(r"^Characters")).click()
+    page.get_by_role("link", name=re.compile(r"^Character Sheet")).click()
     page.locator("#id_writing_assigned").uncheck()
     page.locator("#id_writing_cover").uncheck()
     page.locator("#id_writing_hide").check()
@@ -156,7 +156,7 @@ def feature_fields2(page: Any, live_server: Any) -> None:
 def feature_file(page: Any, live_server: Any) -> None:
     # enable config gating the 'file' question type
     page.get_by_role("link", name="Configuration").first.click()
-    page.get_by_role("link", name=re.compile(r"^Characters")).click()
+    page.get_by_role("link", name=re.compile(r"^Character Sheet")).click()
     page.locator("#id_writing_file").check()
     submit_confirm(page)
 
